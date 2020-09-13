f887824f04431d46bc117716b565662e0081c443
Ives Moreira Gomes <ives.gomes@acensjr.com>
2020-09-13 17:37:06 -0300

    History Adjusted

--- index.js
+++ index.js
@@ -107,11 +107,15 @@ function secondDialog() {
   o foco principal do conselho.
   <br>
   <br>
-  Cornn, a cidade-estado do norte, é governada pelo humano tirano, chamado Harrien. Ele é conhecido por 
+  Cornn, a cidade-estado do norte, é governada por um humano, chamado Harrien. Ele é conhecido por 
   sua crueldade e muitas pessoas o temem. A cidade possui bastante comércio e você pode encontrar 
   de tudo por lá. Por lá circulam desde criminosos, praticantes de magia proibida até andarilhos. 
   Isso tudo porque o tirano só se importa com ele mesmo e muitos guerreiros e magos de magia proibida 
-  são seus seguidores, sendo quase impossível tirar ele do poder...
+  são seus seguidores, sendo quase impossível tirar ele do poder.
+  <br>
+  <br>
+  No meio das cidades-estado, temos uma vasta e densa floresta. Ela não é muito explorada e possui 
+  uma grande estrada ligando as cidades. Dizem que coisas estranhas acontecem naquela floresta...
   `;
 
   const button = document.createElement('button');
